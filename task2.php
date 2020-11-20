26619a691f023a713b2ddfca3e7b487a43bf5241
Alexander Veats <ajveats@gmail.com>
2020-11-20 16:38:05 +0000

    Continued working and cleaning up of product management console.

--- task2.php
+++ task2.php
@@ -16,13 +16,22 @@
 
     $query_string = "SELECT * from products;";
     $result = mysqli_query($connection, $query_string);
+
     if(!$connection) {
         echo "<p>Initalising MySQLi failed</p>";
-    } else {
+    }
+
+    if (isset($_POST['viewProducts'])) {
         echo "<h2>All products in the products database</h2>";
+
         while($row = mysqli_fetch_assoc($result)) {
             $id = $row['id'];
             $name = $row['name'];
+            $description = $row['description'];
+            $price = $row['price'];
+            $costPrice = $row['cost_price'];
+            $stock = $row['stock'];
+            $ean = $row['ean'];
             echo "<ul>";
             echo "<li>Id: ".$row['id']."</li>";
             echo "<li>Name: ".$row['name']."</li>";
@@ -31,34 +40,77 @@
             echo "<li>Cost price: ".$row['cost_price']."</li>";
             echo "<li>Stock: ".$row['stock']."</li>";
             echo "<li>EAN: ".$row['ean']."</li>";
-            echo "<li><a href='?mode=update&id=$id'>Update: ".$name."</a></li>";
+            echo "<li><a href='?mode=update&id=$id&name=$name&description=$description&price=$price&costPrice=$costPrice&stock=$stock&ean=$ean'>Update: ".$name."</a></li>";
             echo "<li><a href='?mode=delete&id=$id'>Delete: ".$name."</a></li>";
             echo "</ul>";
         }
     }
+        $productUnselectedForm = '
+        <h2>Add a product to the catalogue</h2>
+        <form action="task2.php" method="POST">
+            <label>Enter product name:<label>
+            <input type="text" name="name" required><br>
+            <label>Enter product description: <label>
+            <input type="text" name="description" required><br>
+            <label>Enter product price:<label>
+            <input type="text" name="price" required><br>
+            <label>Enter product cost price</label>
+            <input type="text" name="costPrice" required><br>
+            <label>Enter product stock</label>
+            <input type="text" name="stock" required><br>
+            <label>Enter product EAN</label>
+            <input type="text" name="EAN" "required><br>
+            <input type="submit" name="addSubmit" value="Add product">
+            <button type="reset" name="resetForm">Clear form</button>
+        </form>';
+    
+
+        
 ?>
 <h1>Product management console</h1>
+<form action="task2.php" method="POST">
+    <p>Click here to see all the products in the catalogue<input type="submit" name="viewProducts" value="View all products"></p>
+    <!--<input type="submit" name="viewProducts" value="View all products"> -->
+</form>
     <div>
-        <h2>Add a new product or edit a product from the catalogue</h2>
-        <form action="task2.php" method="POST">
-            <label>Product id<label>
-            <input type="text" name="id"><br>
-            <label>Enter product's name:<label>
-            <input type="text" name="name" required><br>
-            <label>Enter product's description: <label>
-            <input type="text" name="description" required><br>
-            <label>Enter product's price:<label>
-            <input type="text" name="price" required><br>
-            <label>Enter product's cost price</label>
-            <input type="text" name="costPrice" required><br>
-            <label>Enter product's stock</label>
-            <input type="text" name="stock" required><br>
-            <label>Enter product's EAN</label>
-            <input type="text" name="EAN" required><br>
-            <input type="submit" name="addSubmit" value="Add product">
-            <input type="submit" name="updateSubmit" value="Edit product">
-            <button type="reset" name="resetForm">Clear form</button>
-       </form>
+        <?php 
+            if (isset($_GET['id'])) {
+                $urlId = $_GET['id'];
+                $urlName = $_GET['name'];
+                $urlDescription  = $_GET['description'];
+                $urlPrice  = $_GET['price'];
+                $urlCostPrice = $_GET['costPrice'];
+                $urlStock = $_GET['stock'];
+                $urlEan = $_GET['ean'];
+
+                $productSelectedForm = '
+                <h2>Edit <?php echo $urlName; ?> product from the catalogue</h2>
+                <form action="task2.php" method="POST">
+                <label>Product id<label>
+                <input type="text" name="id" value="<?php echo $urlId; ?>"><br>
+                <label>Enter product name:<label>
+                <input type="text" name="name" value="<?php echo $urlName; ?>" required><br>
+                <label>Enter product description: <label>
+                <input type="text" name="description" value="<?php echo $urlDescription ?>" required><br>
+                <label>Enter product price:<label>
+                <input type="text" name="price" value="<?php echo $urlPrice; ?>" required><br>
+                <label>Enter product cost price</label>
+                <input type="text" name="costPrice" value="<?php echo $urlCostPrice; ?>"required><br>
+                <label>Enter product stock</label>
+                <input type="text" name="stock" value="<?php echo $urlStock; ?>"required><br>
+                <label>Enter product EAN</label>
+                <input type="text" name="EAN" value="<?php echo $urlEan; ?>"required><br>
+                <input type="submit" name="updateSubmit" value="Edit product">
+                <button type="reset" name="resetForm">Clear form</button>
+                </form>';
+
+                echo $productSelectedForm;
+                //echo $_GET['id'];
+            } else {
+                echo '<script>console.log("Click a product"); </script>';
+                echo $productUnselectedForm;
+            }
+        ?>
     </div>
 
 <?php
@@ -90,6 +142,7 @@
 
     if (isset($_POST['updateSubmit'])) {
         $editproductID = $_POST['id'];
+        echo $editProductID;
         $editProductName = $_POST['name'];
         $editProductDescription = $_POST['description'];
         $editProductPrice = $_POST['price'];
@@ -97,9 +150,7 @@
         $editproductStock = $_POST['stock'];
         $editproductEAN = $_POST['EAN'];
 
-        $edit_query_string = "UPDATE products 
-                            SET name = '$editProductName', description = '$productDescription', price = '$editProductPrice', cost_price = '$productCostPrice', stock = '$editproductStock', ean = '$editproductEAN'
-                            WHERE id = '$editproductID';";
+        $edit_query_string = "UPDATE products SET name = '$editProductName', description = '$productDescription', price = '$editProductPrice', cost_price = '$productCostPrice', stock = '$editproductStock', ean = '$editproductEAN' WHERE id = '$editproductID';";
         
         $result = mysqli_query($connection, $edit_query_string);
 
